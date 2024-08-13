e14489fb68897bd68e14eb20112abb3704accd77
FelixMontesF <123341578+FelixMontesF@users.noreply.github.com>
2024-08-13 15:47:20 -0700

    Create Backendcoding.java

---
+++ Backendcoding.java
@@ -0,0 +1,6 @@
+//Back end coding
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
