24ec728a2d85aeb57bb6afefe721bc4e66589e1e
Thameme03 <150420165+Thameme03@users.noreply.github.com>
2024-08-14 23:33:08 -0700

    Update roomService.java

--- roomService.java
+++ roomService.java
@@ -1,13 +1,32 @@
+/**
+*Room Service class
+*@since 8/6/2024
+*@author Thanim Uddin
+*The roomService class takes care of the filtering function for guests wanting a specific room
+*/
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class roomService {
     private List<room> rooms;
 
+    /**
+    *Constructor to create a RoomService with a list of rooms
+    *@param rooms    The list of rooms available in the hotel
+    */
+    
     public roomService(List<room> rooms) {
         this.rooms = rooms;
     }
 
+    /**
+    *Filters available rooms based on the specified criteria given by the user
+    *@param type      The type of the room (Single, Double, Suite)
+    *@param minPrice  The minimum price per night for the room
+    *@param maxPrice  The maximum price per night for the room
+    *@param guests    The maximum number of guests that the room can have
+    */
+    
     public List<room> filterRooms(String type, double minPrice, double maxPrice, int guests) {
         return rooms.stream()
                 .filter(room -> room.getType().equalsIgnoreCase(type))
