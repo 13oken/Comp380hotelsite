7927b3852b1096695ab43dafd40c336e969cece1
FranciscoR0 <frankie.orellana02@gmail.com>
2024-08-14 22:07:49 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,45 @@
+// Get the HTML
+const contactForm = document.querySelector('.contact-form');
+
+// Get individual input fields from the form using their IDs.
+let fullname = document.getElementById('name');
+let email = document.getElementById('email');
+let subject = document.getElementById('subject');
+let message = document.getElementById('message');
+
+
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault(); 
+
+    // Store
+    let formData = {
+        name: fullname.value, 
+        email: email.value, 
+        subject: subject.value, 
+        message: message.value, 
+    };
+
+    
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/'); 
+    xhr.setRequestHeader('content-type', 'application/json'); 
+
+   
+    xhr.onload = function() {
+        console.log(xhr.responseText); 
+        if (xhr.responseText == 'success') {
+            alert('Email Sent');   
+
+            // Clear the after submission
+            fullname.value = '';
+            email.value = '';
+            subject.value = '';
+            message.value = '';
+        } else {
+            alert('Something went wrong!'); // Show an error if something went wrong
+        }
+    };
+
+    
+    xhr.send(JSON.stringify(formData));
+});
---
+++ web.js
@@ -0,0 +1,59 @@
+const { execFile } = require('child_process');
+
+const express = require('express');
+const web = express(); 
+
+const nodemailer = require("nodemailer");
+
+const server = process.env.PORT || 9000;
+
+web.use(express.static('public')); 
+web.use(express.json()); 
+
+web.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/contact.html'); // Send the contact form HTML
+});
+
+web.post('/', (req, res) => {
+    console.log(req.body); 
+
+   
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'codesign23@gmail.com', // The Gmail account used to send the email.
+            pass: '1234 1234 1234 5678', // Password for above
+        }
+    });
+
+    
+    const mailOptions = {
+        from: req.body.email, // The sender's email address from the form.
+        to: 'codesign23@gmail.com', // The email address to receive the form data.
+        subject: 'Contact Form ', // Subject line for the email.
+        html: `
+            <h2 style="color: #333;">Contact Form</h2>
+            <p><strong>Name:</strong> ${req.body.name}</p>
+            <p><strong>Email:</strong> ${req.body.email}</p>
+            <p><strong>Subject:</strong> ${req.body.subject}</p>
+            <p><strong>Message:</strong><br>${req.body.message}</p>
+        `
+    };
+
+    
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error); 
+            res.status(500).send('Error sending email'); 
+        } else {
+            console.log('Email sent: ' + info.response); 
+            res.status(200).send({ status: 'success' }); 
+        }
+    });
+
+});
+
+// Start the sever
+web.listen(server, () => {
+    console.log(`http://localhost:${server}`); // Print the server URL 
+});
