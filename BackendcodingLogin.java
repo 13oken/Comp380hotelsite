e78bb58839fd60b34103d5c3b9264e8a63fe8935
FelixMontesF <123341578+FelixMontesF@users.noreply.github.com>
2024-08-14 22:56:03 -0700

    Update BackendcodingLogin.java

--- BackendcodingLogin.java
+++ BackendcodingLogin.java
@@ -1,4 +1,25 @@
 //Back end coding for the Login
+
+
+//3.2 a-e
+
+// (a) Class name for this code is login for the fx
+
+//  (b)  8/14/24
+
+// (c) Felix Montes-Flores
+
+// (d) This class is about putting a customer's information on a page which will confirm their credentials
+
+// (e) In the LoginServlet function of this class, a username is requested to be inputted as well as a password.
+// Then, the username and password is validated so that when the system looks at the info, it recognizes it and 
+// confirms them and shows a reservation that they have done.
+
+
+
+
+
+
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
