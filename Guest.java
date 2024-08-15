5670924473983a137d280b01cda24ea10b339fcc
Thameme03 <150420165+Thameme03@users.noreply.github.com>
2024-08-14 23:15:30 -0700

    Update Guest.java

--- Guest.java
+++ Guest.java
@@ -1,32 +1,63 @@
+/**
+*Guest class
+*@since 8/10/2024
+*@author Thanim Uddin
+*Stores the information of a guest who is looking to reserve a room in the hotel. It will store the user's name, email, and phone number.
+*/
 public class Guest {
     private String firstName;
     private String lastName;
     private String email;
     private String phoneNumber;
-
+    
+    /**
+    *Constructor that creates a new Guest using the information provided by the user
+    *@param name the guest's first and last name, email, and phone number
+    */
+    
     public Guest(String firstName, String lastName, String email, String phoneNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.phoneNumber = phoneNumber;
     }
-
+    
+    /**
+    *@return the guest's first name
+    */
+    
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+    *Sets the guest's first name
+    */
+    
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+    *@return the guest's last name
+    */
+    
     public String getLastName() {
         return lastName;
     }
 
+    /**
+    *Sets the guest's last name
+    */
+    
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+    *@return the guest's email
+    */
+    
     public String getEmail() {
         return email;
     }
@@ -35,6 +66,10 @@ public class Guest {
         this.email = email;
     }
 
+    /**
+    *@return the guest's number
+    */
+    
     public String getPhoneNumber() {
         return phoneNumber;
     }
