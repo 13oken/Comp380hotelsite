c23d76c5727601a1d8bda8278536b1fbb20817c2
Thameme03 <150420165+Thameme03@users.noreply.github.com>
2024-08-14 22:19:48 -0700

    Add files via upload

---
+++ Reservation.java
@@ -0,0 +1,41 @@
+import java.time.LocalDate;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Reservation {
+    private int reservationId;
+    private static final AtomicInteger uniqueId = new AtomicInteger(0);
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+    private Guest guest;
+    private room rooms;
+
+    public Reservation(Guest guest, room rooms, LocalDate checkIn, LocalDate checkOut) {
+        reservationId = uniqueId.incrementAndGet();
+        this.guest = guest;
+        this.rooms = rooms;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public int getreservationId() {
+        return reservationId;
+    }
+
+    public LocalDate checkIn() {
+        return checkIn;
+    }
+
+    public LocalDate checkOut() {
+        return checkOut;
+    }
+
+    public Guest guest() {
+        return guest;
+    }
+
+    public room rooms() {
+        return rooms;
+    }
+
+    
+}
